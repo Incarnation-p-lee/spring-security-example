541ee8523a0192a3c487dd23bf4272b79b3f6582
Pan Li <incarnation.p.lee@outlook.com>
2018-05-05 23:42:22 +0800

    Refine WebSecurityConfiguration.
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

--- src/main/java/example/spring/security/controller/WebController.java
+++ src/main/java/example/spring/security/controller/WebController.java
@@ -1,15 +1,21 @@
 package example.spring.security.controller;
 
+import example.spring.security.Constants;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WebController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = Constants.PATH_ROOT, method = RequestMethod.GET)
     public String getHomePage() {
-        return "This is Home page!";
+        return Constants.PAGE_ROOT;
+    }
+
+    @RequestMapping(value = Constants.PATH_LOGIN, method = RequestMethod.GET)
+    public String getLogin() {
+        return Constants.PAGE_LOGIN;
     }
 
 }
