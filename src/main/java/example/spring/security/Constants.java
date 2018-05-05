8302090018beeea814cde916ebc2e5fc899c9c72
Pan Li <incarnation.p.lee@outlook.com>
2018-05-05 23:42:51 +0800

    Add constant class.
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

---
+++ src/main/java/example/spring/security/Constants.java
@@ -0,0 +1,15 @@
+package example.spring.security;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Constants {
+
+    public final static String PATH_LOGIN = "/login";
+    public final static String PAGE_LOGIN = "login";
+
+    public final static String PATH_ROOT = "/";
+    public final static String PAGE_ROOT = "index";
+
+}
